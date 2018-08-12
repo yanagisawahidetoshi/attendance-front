50115ce7f36cbf06ca192ece8d8d7e2d64c01939
YanagisawaHidetoshi <yanagisawa@ultrasevenstar.com>
2018-08-12 22:56:11 +0900

    ログイン実装

---
+++ pages/login.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <el-alert
+      v-if="isFaild"
+      title="メールアドレスもしくはパスワードが違います"
+      type="error">
+    </el-alert>
+    <el-form ref="form" :model="form" label-position="top">
+      <el-form-item label="メールアドレス">
+        <el-input v-model="form.email" type="email" />
+      </el-form-item>
+      <el-form-item label="パスワード">
+        <el-input v-model="form.password" type="password" />
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit">ログイン</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data: function () {
+    return {
+      form: {
+        email: '',
+        password: ''
+      },
+    }
+  },
+  computed: {
+    isFaild () {
+      return this.$store.state.isLoginFaild
+    }
+  },
+  methods: {
+    onSubmit () {
+      const params = this.form
+      this.$store.dispatch('login', this.form)
+    }
+  }
+}
+</script>
+
+<style>
+</style>
+
---
+++ store/index.js
@@ -0,0 +1,25 @@
+import axios from 'axios'
+export const state = () => ({
+  isLoginFaild: false,
+  currentUser: {}
+})
+export const actions = {
+  async login (context, params) {
+    try {
+      const res = await this.$axios.$post('/v1/login', params)
+      context.commit('setCurrentUser', true)
+      context.commit('setIsLoginFaild', false)
+    } catch(e) {
+      context.commit('setIsLoginFaild', true)
+    }
+  }
+}
+
+export const mutations = {
+  setIsLoginFaild (state, value) {
+    state.isLoginFaild = value 
+  },
+  setCurrentUser (state, user) {
+    state.currentUser = user 
+  }
+}
